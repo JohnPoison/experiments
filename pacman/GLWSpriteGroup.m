ab73fca2d77d7e8b708a2b30561d74b5cf38f944
JohnPoison <truefiresnake@gmail.com>
2013-03-28 03:18:38 +0400

    removed shit

--- pacman/GLWSpriteGroup.m
+++ pacman/GLWSpriteGroup.m
@@ -9,10 +9,6 @@
 #import "GLWShaderManager.h"
 #import "GLWSprite.h"
 
-static const GLushort indices[] = {
-    0, 1, 2, 3, 2, 1
-};
-
 static const int VertexSize = sizeof(GLWVertexData);
 
 @implementation GLWSpriteGroup {
@@ -21,36 +17,7 @@ static const int VertexSize = sizeof(GLWVertexData);
 - (id)init {
     self = [super init];
     if (self) {
-//        _vertices = malloc(sizeof(GLWVertex4Data));
-
-        GLWVertex4Data data;
-
-        GLWVertexData tl;
-        GLWVertexData tr;
-        GLWVertexData bl;
-        GLWVertexData br;
-
-        Vec4 color;
-        color.x = 0;
-        color.y = 255;
-        color.z = 0;
-        color.w = 255;
-
-        tl.color = tr.color = bl.color = br.color = color;
-
-        tl.vertex = Vec3Make(0.f, 100.f, 0.f);
-        tr.vertex = Vec3Make(100.f, 100.f, 0.f);
-        bl.vertex = Vec3Make(0.f, 0.f, 0.f);
-        br.vertex = Vec3Make(100.f, 0.f, 0.f);
-
-        data.topLeft        = tl;
-        data.topRight       = tr;
-        data.bottomLeft     = bl;
-        data.bottomRight    = br;
-
-//        vertices[0] = data;
         glGenBuffers(2, vboIds);
-
         isDirty = YES;
     }
 
